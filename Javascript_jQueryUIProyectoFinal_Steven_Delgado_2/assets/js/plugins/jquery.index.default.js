f6aeb6bb52c6bb10216c31139c221372eadb0072
Steven Delgado <steven.delgado@gmail.com>
2016-09-19 23:30:41 -0300

    modificación de aplicación de damas chinas

---
+++ Javascript_jQueryUIProyectoFinal_Steven_Delgado_2/assets/js/plugins/jquery.index.default.js
@@ -0,0 +1,25 @@
+/**
+ * @author [Steven Delgado Ch] <[< steven.delgado.ch@gmail.com >]>
+ * @description [  jquery.index.default.js ]
+ * Fecha de creación: 07/08/16
+ * Fecha de actualización:
+ * Participantes: [ ]
+ */
+$(function() {
+
+	'use strict';
+
+
+	$.fn.addMoveCheckers = function() {
+
+
+
+		console.log(this)
+
+
+
+	};
+
+
+
+}(jQuery));
