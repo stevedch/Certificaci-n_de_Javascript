5410b93b248f148457e2d14c0b2215e13f378ccb
Steven Delgado <steven.delgado.ch@gmail.com>
2016-10-15 15:08:32 -0300

    Jquery mobile

---
+++ Javascript_jQueryMobileProyectoFinal_Steven_Delgado/js/index.default.load.js
@@ -0,0 +1,20 @@
+/**
+ * @author [Steven Delgado Ch] <[< steven.delgado.ch@gmail.com >]>
+ * @description [ Este script realiza las siguientes acciones:
+ * - Almacenamiento de datos de un estudiante [ localStorage ].
+ * - Mostrar todos los objetos del json.
+ * - Calcular la nota promedio y mostrar en pantalla.
+ * - Mostrar en pantalla el estudiante con la mayor nota.
+ * - Mostrar en pantalla el estudiante con la menor nota de todas.
+ * ]
+ * Fecha de creación: 03/07/16
+ * Fecha de actualización:
+ * Participantes: [ ]
+ */
+;
+(function($, window, document, undefined) {
+
+	'use strict';
+
+
+}(jQuery, window, document));
